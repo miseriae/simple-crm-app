334a701d59aa4c089c872e41f1aa67f6d4506ae3
miseriae <zuyev.frm@gmail.com>
2020-02-27 15:44:09 +0200

    Initial Commit

---
+++ crm_main/local_settings.py
@@ -0,0 +1,16 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+secret_key = 'vvgeinli9pmpypf9sap@_c!fd4b04u9dg-c)l&!pp+wuw36z&l'
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
